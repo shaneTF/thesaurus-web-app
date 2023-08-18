cde0e5c4c8d0c75455bc1a5e77c0d3a93f4b242e
Shane Franklin <shanefranklin148@gmail.com>
2023-08-18 10:21:52 -0400

    Added datamuse api to get synonyms in state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { useState } from "react";
 import "./App.css";
 
+type Synonyms = {
+  word: string;
+  score: number;
+}
+
 function App() {
   const [word, setWord] = useState("");
+  const [synonyms, setSynonyms] = useState<Synonyms[]>([]);
+
+  const handleFetchSynonyms = (e: React.FormEvent) => {
+    e.preventDefault();
+    fetch(`https://api.datamuse.com/words?rel_syn=fast`)
+    .then((response) => response.json())
+    .then(setSynonyms);
+  }
 
   return (
     <>
       <div className="App">
-        <form>
+        <form onSubmit={handleFetchSynonyms}>
           <label htmlFor="word-input">Your Word</label>
           <input
             value={word}
